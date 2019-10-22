bcbd1a0132d1e56bbb59e5db1e3c93eed75c0fba
enzu <jiangao@centaurstech.com>
2019-10-22 15:49:56 +0800

    更改保存上次使用rule名的条件

--- rule-editor/static/js/tool.js
+++ rule-editor/static/js/tool.js
@@ -805,7 +805,8 @@ $(function () {
         if (saveSpace.val().indexOf(rule.val()) === -1) {
             saveSpace.val(saveSpace.val() + "\n" + rule.val());
             window.localStorage.setItem("rulesData", saveSpace.val());
-            if (ruleNameLastTimeUsed === window.localStorage.getItem("ruleNameLastTimeUsed")) {
+            alert(ruleNameLastTimeUsed +"----"+ window.localStorage.getItem("ruleNameLastTimeUsed"));
+            if (ruleNameLastTimeUsed !== window.localStorage.getItem("ruleNameLastTimeUsed")) {
                 window.localStorage.setItem("ruleNameLastTimeUsed", ruleNameLastTimeUsed);
             }
         } else {
