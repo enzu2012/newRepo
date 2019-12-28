0dc2f6d97aea313e317a5a370e791118e49253f9
enzu <jiangao@centaurstech.com>
2019-12-28 17:50:32 +0800

    完成规则非法字符检测功能，正则检测暂未开发

--- rule-editor/static/js/findWrongRules.js
+++ rule-editor/static/js/findWrongRules.js
@@ -1,36 +1,87 @@
-//加载完毕执行
+
 function findWrongRules(rules){
     var rules = rules.split("\n");
-    //alert(rules.length);
+    var wrongInfo="";
+    var wrongStatus=0;
+    var illegalStringObj=[
+        {
+            illegalString:"|+",wrongMsg:"非法字符组合《|+》"
+        },{
+            illegalString:"+==",wrongMsg:"非法字符组合《+=》"
+        },{
+            illegalString:",==",wrongMsg:"非法字符组合《,=》"
+        },{
+            illegalString:"+++",wrongMsg:"非法字符组合《+++》"
+        },{
+            illegalString:"《=%",wrongMsg:"非法字符组合《=%》"
+        },{
+            illegalString:"=*",wrongMsg:"非法字符组合《=*》"
+        },{
+            illegalString:"][[",wrongMsg:"非法字符组合《][[》"
+        },{
+            illegalString:"[[",wrongMsg:"非法字符组合《[[》"
+        },{
+            illegalString:"][",wrongMsg:"非法字符组合《][》"
+        },{
+            illegalString:",=",wrongMsg:"非法字符组合《,=》"
+        },{
+            illegalString:"|+",wrongMsg:"非法字符组合《|+》"
+        },{
+            illegalString:"|=",wrongMsg:"非法字符组合《|=》"
+        },{
+            illegalString:"##",wrongMsg:"非法字符组合《##》"
+        },{
+            illegalString:",,",wrongMsg:"非法字符组合《,,》"
+        },{
+            illegalString:"《..",wrongMsg:"非法字符组合《..》"
+        },{
+            illegalString:".,",wrongMsg:"非法字符组合《.,》"
+        },{
+            illegalString:",.",wrongMsg:"非法字符组合《,.》"
+        },{
+            illegalString:"[N]",wrongMsg:"非法字符组合《[N]》"
+        },{
+            illegalString:"%%",wrongMsg:"非法字符组合《%%》"
+        },{
+            illegalString:"**",wrongMsg:"非法字符组合《**》"
+        },{
+            illegalString:"=+,",wrongMsg:"非法字符组合《=+,》"
+        }];
+    var checkRegExpObj= [
+        {
+            regString:"~[^~]*~",wrongMsg:"存在2个《~》符号"
+        },{
+            regString:"\|[^\|]*\|",wrongMsg:"存在2个《|》符号"
+        },{
+            regString:"\\.(?:(?:^#)|\\n)",wrongMsg:"符号《.》后面不是#或者rule结尾"
+        }];
     for (var i = 0; i < rules.length; i++) {
         if (rules[i]) {
-            if (rules[i].indexOf("|") !== -1) {
-                rules[i] = rules[i].split("|")[0];
-            } else if (rules[i].indexOf("~") !== -1) {
-                rules[i] = rules[i].split("~")[0];
-            } else {
-                rules[i] = "";
+            for( var str=0;str<illegalStringObj.length;str++){
+                if (rules[i].indexOf(illegalStringObj[str].illegalString)!==-1) {
+                    if(wrongStatus===0){
+                        wrongInfo+=rules[i]+"【"+illegalStringObj[str].wrongMsg+"】";
+                        wrongStatus=1;
+                    }else{
+                        wrongInfo+="【"+illegalStringObj[str].wrongMsg+"】";
+                    }
+                }
             }
-            rules[i] = rules[i].replace(/\[[^\]]*\]/gi, "");
-            rules[i] = rules[i].replace(/QA:/gi, "");
-            //alert(rules[i]);
+            /*此处增加正则检测逻辑*/
+            if(wrongStatus===1) {
+                wrongInfo += "\n\n";
+            }
+            wrongStatus=0;
         }
     }
-    fileOfRules = "";
-    for (var j = 0; j < rules.length; j++) {
-        if (rules[j]) {
-            fileOfRules += rules[j] + "\n";
-        }
-    }
-
     return wrongInfo;
 }
 
 
-
+//加载完毕执行
 $(function () {
-    $("button#btn-find-wrong-rules").on("click",function () {
-        var wrongInfo=findWrongRules($("textarea#rules").val());
-        $("textarea#wrong-rules").val(wrongInfo);
+    $("input#btn-find-wrong-rules").on("click",function () {
+        var wrongRuleInfo=findWrongRules($("textarea#rules").val());
+        $("textarea#wrong-rules").val(wrongRuleInfo);
     })
 });
